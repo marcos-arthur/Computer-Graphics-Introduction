cd389549da6cc4fff8006836a6899a7518fa9eee
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2026-08-07 17:50:39 -0300

    feature: last work

--- script.sh
+++ script.sh
@@ -5,11 +5,43 @@ mkdir -p frames
 cycles=2
 
 scenes=(
-    "scenes/boneco_1.xml"
-    "scenes/boneco_2.xml"
-    "scenes/boneco_3.xml"
-    "scenes/boneco_2.xml"
-    "scenes/boneco_3.xml"
+    "scenes/race_1.xml"
+    "scenes/race_2.xml"
+    "scenes/race_3.xml"
+    "scenes/race_4.xml"
+    "scenes/race_5.xml"
+    "scenes/race_6.xml"
+    "scenes/race_7.xml"
+    "scenes/race_8.xml"
+    "scenes/race_9.xml"
+    "scenes/race_10.xml"
+    "scenes/race_11.xml"
+    "scenes/race_12.xml"
+    "scenes/race_13.xml"
+    "scenes/race_14.xml"
+    "scenes/race_15.xml"
+    "scenes/race_16.xml"
+    "scenes/race_17.xml"
+    "scenes/race_18.xml"
+    "scenes/race_19.xml"
+    "scenes/race_20.xml"
+    "scenes/race_21.xml"
+    "scenes/race_22.xml"
+    "scenes/race_23.xml"
+    "scenes/race_24.xml"
+    "scenes/race_25.xml"
+    "scenes/race_26.xml"
+    "scenes/race_27.xml"
+    "scenes/race_28.xml"
+    "scenes/race_29.xml"
+    "scenes/race_30.xml"
+    "scenes/race_31.xml"
+    "scenes/race_32.xml"
+    "scenes/race_33.xml"
+    "scenes/race_34.xml"
+    "scenes/race_35.xml"
+    "scenes/race_36.xml"
+    "scenes/race_37.xml"
 )
 
 frame_count=0
@@ -17,12 +49,12 @@ frame_count=0
 for ((i=0; i<cycles; i++)); do
     for ((j=0; j<${#scenes[@]}; j++)); do
         ./build/PEinT "${scenes[$j]}"
-        mv boneco.png "frames/frame_$(printf "%03d" "$frame_count").png"
+        mv race.png "frames/frame_$(printf "%03d" "$frame_count").png"
         ((frame_count++))
     done
 done
 
-frame_rate=3
+frame_rate=12
 
 ffmpeg -framerate $frame_rate \
     -i frames/frame_%03d.png \
